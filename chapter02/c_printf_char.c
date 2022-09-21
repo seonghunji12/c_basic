96c5740158afdc630dcf40ac6d2136f6d51b0b0b
seonghunji12 <112991524+seonghunji12@users.noreply.github.com>
2022-09-21 10:48:37 +0900

    finished chapter02

---
+++ chapter02/c_printf_char.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+// vs code 단축기
+//shift + alt + ↑ or ↓ :라인복사  (복제)
+//ctrl + alt + n : c실행
+// alt + ↑ or ↓ : 라인이동
+//ctrl + x : line삭제
+// ctrl + space : 자동 완성
+
+// '' : 문자(character) 'A', 'B', '!'
+//" " : 문자열(STRING) "A", "ABD", "A!"
+
+// ※ 문자('')를 사용하는 경우
+// +키보드 제어를 할 때 사용
+int main(void)
+{
+    /* code */
+    printf("%c\n , 'A'");
+    printf("%s\n , 'A'");
+    printf("%c은 %s입니다.\n", '1', "first");
+    return 0;
+}
